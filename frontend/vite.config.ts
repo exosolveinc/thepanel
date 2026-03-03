51ac8386bc619a8e19392726188d2e2e87935091
Sariph Shrestha <sariph@Sariphs-MacBook-Air.local>
2026-03-03 17:36:54 -0500

    Added changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': 'https://thepanel-production.up.railway.app',
     },
   },
 })
